d55625c0358713b2a28657aab8e673c4441548f4
JAN REY SUAREZ <w33haa2@gmail.com>
2021-10-09 23:09:42 +0800

    migration stuffs

--- database/migrations/2021_10_09_150730_alter_user_table.php
+++ database/migrations/2021_10_09_150730_alter_user_table.php
@@ -14,7 +14,7 @@ class AlterUserTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('companyname')->nullable();
+            $table->string('companyname')->nullable()->change();
         });
     }
 
