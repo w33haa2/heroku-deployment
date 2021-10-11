c16e0e116cb204a251973c096acf1f01b4b8b09e
JAN REY SUAREZ <w33haa2@gmail.com>
2021-10-11 23:06:28 +0800

    seeder stuff

---
+++ database/seeds/CampaignSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\EmailCampaign;
+class ProspectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $arr = [
+            [
+                'name' => 'Hardware Email Template',
+                'subject' => 'Promotion',
+                'contact' => 'Hello',
+            ],
+            [
+                'name' => 'IT Email Template',
+                'subject' => 'Promotion',
+                'contact' => 'Hello',
+            ],
+            [
+                'name' => 'Test Email Template',
+                'subject' => 'Promotion',
+                'contact' => 'Hello',
+            ],
+            [
+                'name' => 'Jersey Promotion',
+                'subject' => 'Promotion',
+                'contact' => 'Hello',
+            ],
+            [
+                'name' => 'Frogkaffee',
+                'subject' => 'Frogkaffee',
+                'contact' => 'Hello',
+            ],
+        ];
+        foreach($arr as $i => $value) {
+            EmailCampaign::create($value);
+        }
+    }
+}
